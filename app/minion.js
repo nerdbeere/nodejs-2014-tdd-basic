27ca1422bb66f98c7bd2d8b42c7814cb4e47faa2
Julian Hollmann <julian.hollmann@googlemail.com>
2014-03-10 18:33:29 +0100

    working logic

--- app/minion.js
+++ app/minion.js
@@ -5,19 +5,26 @@ var Minion = function(x, y) {
 };
 
 Minion.prototype.move = function(x, y) {
-
+	this.pos.x += x;
+	this.pos.y += y;
 };
 
 Minion.prototype.moveTo = function(x, y) {
-	
+	this.pos.x = x;
+	this.pos.y = y;
 };
 
 Minion.prototype.applyDamage = function(amount) {
-
+	this.health -= amount;
+	if(this.health <= 0) {
+		this.health = 0;
+		this.dead = true;
+	}
 };
 
 Minion.prototype.resurrect = function() {
-
+	this.health = 100;
+	this.dead = false;
 };
 
 module.exports = Minion;
